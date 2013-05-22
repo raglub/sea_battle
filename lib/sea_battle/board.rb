9f2c98efe234a3a8177e8e01174d23feaf0a2632
Michał Szyma <michal@szyma.dev>
2013-05-22 23:43:50 +0200

    Added method #ship_positions

--- lib/sea_battle/board.rb
+++ lib/sea_battle/board.rb
@@ -3,16 +3,69 @@
 class SeaBattle
   # It's Board of game Sea Battle
   class Board
-    def initialize(board)
-      @board = board
+
+    attr_reader :board
+    # board
+    # 1 -> empty field
+    # 2 -> field is part of ship
+    # 4 -> attacked field
+    # 6 -> attacked field and exsist ship
+    def initialize(board = "0"*100)
+      @board = board.split("").map(&:to_i).each_slice(10).to_a
+      @quantity_ships = {1 => 4, 2 => 3, 3 => 2, 4 => 1}
       check_board
+    end
+
+    def ship_positions(row, column)
+      horizontal = horizontal_ship_position(row, column)
+      vertical = vertical_ship_position(row, column)
+      return horizontal if horizontal.size > vertical.size
+      vertical
+    end
+
+    def attack(row, column)
+      true
     end
 
   private
 
     # It should raise error when @board isn't Array
     def check_board
-      raise "The board is not Array class" unless @board.class.eql?(Array)
+      raise "The board is not Array class" unless @board.size == 10
+    end
+
+    # it should return array of ship position
+    # for horizontal line with position [row, column]
+    def horizontal_ship_position(row, column)
+      result = []
+      10.times do |position|
+        if is_ship?(position, column)
+          result << [position, column]
+        else
+          break if result.include?([row, column])
+          result = []
+        end
+      end
+      result
+    end
+
+    def is_ship?(row, column)
+      @board[row][column] & 2 == 2
+    end
+
+    # it should return array of ship position
+    # for vertical line with position [row, column]
+    def vertical_ship_position(row, column)
+      result = []
+      10.times do |position|
+        if is_ship?(row, position)
+          result << [row, position]
+        else
+          break if result.include?([row, column])
+          result = []
+        end
+      end
+      result
     end
 
   end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,11 +4,39 @@ require "sea_battle"
 
 describe SeaBattle::Board do
 
+  let(:klass) { SeaBattle::Board }
+
   it "should properly initialize class" do
-    expect { SeaBattle::Board.new([]) }.to_not raise_error
+    expect { SeaBattle::Board.new }.to_not raise_error
   end
 
   it "should not properly initialize class" do
     expect { SeaBattle::Board.new("") }.to raise_error
+  end
+
+  context "should check method ship_positions"do
+
+    it "for non-ship" do
+      board = "0"*100
+      board[2] = "2"
+      board[3] = "2"
+
+      klass.new(board).ship_positions(1, 2).should eq([])
+    end
+
+    it "for horizontal ship" do
+      board = "0"*100
+      board[2] = "2"
+      board[3] = "2"
+      klass.new(board).ship_positions(0, 2).should eq([[0, 2], [0, 3]])
+    end
+
+    it "for vertical ship" do
+      board = "0"*100
+      board[23] = "2"
+      board[33] = "2"
+      klass.new(board).ship_positions(2, 3).should eq([[2, 3] ,[3, 3]])
+    end
+
   end
 end
