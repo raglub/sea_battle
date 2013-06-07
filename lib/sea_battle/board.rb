07b88b3ca19f7c1a9034c3682372f974fe6543a6
Michał Szyma <devszyma@gmail.com>
2013-06-07 05:24:00 +0200

    Created class RandomShip

--- lib/sea_battle/board.rb
+++ lib/sea_battle/board.rb
@@ -1,12 +1,13 @@
 # encoding: utf-8
 
 require_relative "cell"
+require_relative "random_ship"
 
 class SeaBattle
   # It's Board of game Sea Battle
   class Board
 
-    attr_reader :board
+    attr_reader :board, :vertical, :horizontal
 
     def initialize(board = "1" * 100)
       @board = board.split("").map do |status|
@@ -19,14 +20,6 @@ class SeaBattle
       check_board
     end
 
-    def add_random_ship(length)
-      if Random.rand(2) == 0
-        add_random_vertical_ship(length)
-      else
-        add_random_vertical_ship(length)
-      end
-    end
-
     def ship_positions(row, column)
       horizontal = horizontal_ship_position(row, column)
       vertical = vertical_ship_position(row, column)
@@ -35,27 +28,6 @@ class SeaBattle
     end
 
   private
-
-    def add_random_vertical_ship(length)
-      position = Random.rand(@horizontal * @vertical)
-      (@horizontal * @vertical).times do |item|
-        if pair_with(position).first + length < @vertical
-          unless is_block_empty?(
-            pair_with(position).first - 1,
-            pair_with(position).last - 1,
-            pair_with(position).first + (length - 1) + 1,
-            pair_with(position).last + 1
-          )
-            length.times do |offset|
-              p "bbb"
-              @board[pair_with(position).first + offset][pair_with(position).last].add_ship
-              return true
-            end
-          end
-        end
-      end
-      nil
-    end
 
     # It should raise error when @board isn't Array
     def check_board
@@ -73,32 +45,6 @@ class SeaBattle
           break if result.include?([row, column])
           result = []
         end
-      end
-      result
-    end
-
-    def is_block_empty?(x1, y1, x2, y2)
-      vertical_range = (x1..x2).to_a & (0..@vertical).to_a
-      horizontal_range = (y1..y2).to_a & (0..@horizontal).to_a
-      vertical_range.each do |vertical|
-        horizontal_range.each do |horizontal|
-          return false if @board[vertical][horizontal].is_in_ship?
-        end
-      end
-      true
-    end
-
-    def pair_with(position)
-      @ordered_pairs ||= generate_ordered_pairs
-      @ordered_pairs[position]
-    end
-
-    def generate_ordered_pairs
-      result = {}
-      (0...@vertical).to_a.product(
-        (0...@horizontal).to_a
-      ).each_with_index do |array, index|
-        result[index] = array
       end
       result
     end
