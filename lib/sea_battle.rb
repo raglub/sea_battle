628c1d6cdceebf458dd2334a4393121bcc87ac64
Michał Szyma <michal@szyma.dev>
2013-07-24 22:31:57 +0200

    Added method active_user into class SeaBattle

--- lib/sea_battle.rb
+++ lib/sea_battle.rb
@@ -4,28 +4,62 @@ require "sea_battle/gui"
 
 class SeaBattle
 
-  attr_reader :last_move
-
-  def initialize(board_zero, board_one, last_move = nil)
-    @last_move = last_move
-    @board_zero = board_zero
-    @board_one = board_one
+  def initialize(first_board, second_board, last_attack_move = nil)
+    @last_attack_move = last_attack_move.split(";") unless last_attack_move.nil?
+    @first_board = first_board
+    @second_board = second_board
   end
 
-  def attack(player, row, column)
-    return unless status_zero.eql?(:active) and status_one.eql?(:active)
-    return if player == 0 and last_move == nil
-    @last_move = [player, row, column].join(";")
+  def active_user
+    return :first_player if @last_attack_move.nil?
+    player, row, column = @last_attack_move
+    if player.to_sym == :first_player
+      if @second_board.is_in_ship?(row.to_i, column.to_i)
+        return :first_player
+      else
+        return :second_player
+      end
+    else
+      if @first_board.is_in_ship?(row.to_i, column.to_i)
+        return :second_player
+      else
+        return :first_player
+      end
+    end
+  end
+
+  def last_attack_move
+    return if @last_attack_move.nil?
+    @last_attack_move.join(";")
+  end
+
+  def move(player, type, row, column)
+    return false unless [:first_player, :second_player].include?(player)
+    return false unless [:choose, :attack, :mark].include?(type)
+
+    case type
+    when :attack
+      return false unless active_user == player
+      return false unless first_status.eql?(:activated) and second_status.eql?(:activated)
+      if player == :first_player
+        @first_board.attacked(row, column)
+      else
+        @second_board.attacked(row, column)
+      end
+      @last_attack_move = [player, row, column]
+    else
+    end
+    true
   end
 
   private
 
-    def status_zero
-      @board_zero.status
+    def first_status
+      @first_board.status
     end
 
-    def status_one
-      @board_one.status
+    def second_status
+      @second_board.status
     end
 
 end
